42e112101d3f5c74e773eeea5165172af8729f51
Danilo Da Cruz <danilo.s_95@outlook.com>
2024-12-03 10:07:09 -0300

    BeforeAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,7 +1,23 @@
 import { sum } from "./server"
 
-test("sum of 3 + 7 must be 10", () => {
-  const result = sum(3,7)
-  
-  expect(result).toBe(10)
+describe("sum", () => {
+ let sumResult: number
+
+  beforeAll(() => {
+    sumResult = 10
+  })
+
+  it("should do sum of 3 + 7 must be 10", () => {
+    const result = sum(3,7)
+    
+    
+    expect(result).toBe(10)
+  })
+
+  test("sum of 2 + 2 must not be 4", () => {
+    const result = sum(2, 2)
+    
+    expect(result).toBe(4)
+  })
 })
+
